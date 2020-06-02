c58a63752e598b7a3726f99d624b8f39a99d7746
Your Name <e@w.com>
2020-06-02 16:35:23 +0500

    start car application

---
+++ Car application/app.js
@@ -0,0 +1,40 @@
+const car = (name, model, owner, year, image, phone) => ({name, model, owner, year, image, phone});
+const cars = [
+    car('Mersedes', 'Bens','Mariya',2015, 'img/mers.jpg','+7 983 631-01-77'),
+    car('BMW', 'X6','Ivan',2016, 'img/bmw.jpg','+7 904 631-00-77'),
+    car('Hyundai', 'Solaris','Den',2011, 'img/hunday.jpg','+7 983 631-01-00'),
+    car('KIA', 'Optima','Max',2010, 'img/kia.jpg','+7 983 981-54-77'),
+    car('Lexus', 'RX-350','Dmitriy',2017, 'img/lexus.jpg','+7 914 055 40 80'),
+    car('Mazda', 'RX-7','Vladimir',2012, 'img/mazda.jpg','+7 983 666-01-00'),
+    car('Wolksvagen', 'Passat','Timur',2014, 'img/wolksvagen.jpg','+7 983 631-54-54')
+]
+
+new Vue({
+    el: '#app',
+    data: {
+        cars: cars,
+        car: cars[1],
+        selectedCarIndex: 0,
+        phoneVisibility: false,
+        search: '',
+        modalVisibility: false
+    },
+    methods: {
+        selectCar(index){
+            this.car = this.filteredCars[index];
+            this.selectedCarIndex = index;
+        }
+    },
+    computed: {
+        phoneBtnText(){
+            return this.phoneVisibility? 'Hide phone' : 'Show phone';
+        },
+        filteredCars(){
+            return this.cars.filter(car=>{
+                return (car.name).toLowerCase().indexOf(this.search.toLowerCase())>-1||(car.model).toLowerCase().indexOf((this.search).toLowerCase())>-1;
+            });
+        },
+
+    }
+
+})
